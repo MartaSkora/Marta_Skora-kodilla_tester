0ca275ba6575b186617f6b0982458ba9a3baeac3
Marta Skora <Martaskora1@gmail.com>
2023-08-08 20:45:44 +0200

    1.6 Task

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,6 +1,9 @@
 public class FirstClass {
     public static void main(String[] args) {
-        String introduction = "Hello from FirsClass!";
-        System.out.println(introduction);
+        System.out.println("Hello from FirstClass!");
+
+        if (1 > 2){
+            System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
+        }
     }
 }
