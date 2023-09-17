935e4e467ad71797ebd2bca2d4d1245a4bb7d55e
Marta Skóra <Martaskora1@gmail.com>
2023-09-17 20:35:54 +0200

    3.3. Task

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -8,5 +8,28 @@ public class Application {
         int b = 8;
         int sumResult = calculator.sum(a, b);
         boolean correct = ResultChecker.assertEquals(13, sumResult);
+        if (correct) {
+            System.out.println("Metoda sum działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+
+        int subtractResult = calculator.subtract(a, b);
+        boolean correctSubtractResult = ResultChecker.assertEquals(-3, subtractResult);
+        if (correctSubtractResult) {
+            System.out.println("Metoda odejmująca działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda odejmująca nie działa poprawnie dla licz " + a + " i " + b);
+        }
+
+        int powResult = (int) Math.pow(a, b);
+        boolean correctPowResult = ResultChecker.assertEquals(390625, powResult);
+        if (correctPowResult) {
+            System.out.println("Metoda podnoszącą liczbę całkowitą do kwadratu działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda podnoszącą liczbę całkowitą do kwadratu nie działa poprawnie dla liczb " + a + " i " +b);
+        }
+
+
     }
 }
