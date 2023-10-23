e411e1ac28adfa8c87a1fdf4d168b43226dd3f5c
Marta Skóra <Martaskora1@gmail.com>
2023-10-23 21:36:44 +0200

    4.1

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
@@ -0,0 +1,35 @@
+package com.kodilla.collections.arrays.homework;
+
+import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ford;
+import com.kodilla.collections.interfaces.homework.Mazda;
+import com.kodilla.collections.interfaces.homework.Opel;
+
+import java.util.Random;
+
+public class CarsApplication {
+
+    private static final Random RANDOM = new Random();
+
+    public static void main(String[] args) {
+        Car[] cars = new Car[RANDOM.nextInt(15) + 1];
+        for (int n = 0; n < cars.length; n++)
+            cars[n] = drawCar();
+        for (Car car : cars)
+            CarUtils.describeCar(car);
+        }
+
+    public static Car drawCar() {
+
+            int drawnCar = RANDOM.nextInt(3);
+            int speed = RANDOM.nextInt(51);
+
+            if (drawnCar == 0)
+                return new Mazda(speed);
+            else if (drawnCar == 1)
+                return new Ford(speed);
+            else {
+                return new Opel(speed);
+            }
+    }
+}
