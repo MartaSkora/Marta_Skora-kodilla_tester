7e2fbdca68e5b259f3eed142c13c3ff93e1ed643
Marta Skóra <Martaskora1@gmail.com>
2023-11-06 20:39:00 +0100

    6.4

--- kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -3,6 +3,7 @@ package com.kodilla.exception.homework;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Warehouse {
@@ -15,14 +16,14 @@ public class Warehouse {
     public void addOrder(Order order) {
         warehouseOrders.put(order.getNumber(), order);
     }
-    public Order getOrder (String number) throws OrderDoesntExistException {
-        List<Order> ordersToReturn = warehouseOrders.entrySet()
+    public Order getOrder(String number) throws OrderDoesntExistException {
+        Optional<Order> optionalOrder = warehouseOrders.entrySet()
                 .stream()
-                .filter(order -> order.getKey().equals(number))
-                .map(order -> order.getValue())
-                .collect(Collectors.toList());
-        if (ordersToReturn.size() > 0)
-            return ordersToReturn.get(0);
-        throw new OrderDoesntExistException();
+                .filter(entry -> entry.getKey().equals(number))
+                .map(Map.Entry::getValue)
+                .findFirst();
+
+        // return optionalOrder.orElseThrow(OrderDoesntExistException::new);
+        return optionalOrder.orElseThrow(() -> new OrderDoesntExistException());
     }
  }
