c8ecf737f7149d2c7d9e4585f34bbdcf41ea41c1
Marta Skóra <Martaskora1@gmail.com>
2023-12-04 20:43:46 +0100

    9.4

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/Car.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/Car.java
@@ -3,7 +3,8 @@ package com.kodilla.spring.basic.spring_configuration.homework;
 import java.time.LocalTime;
 
 public interface Car {
-    public static boolean hasHeadlightsTurnedOn(LocalTime time){
+    public static boolean hasHeadlightsTurnedOn(){
+        LocalTime time = LocalTime.now();
         return time.isAfter(LocalTime.of(20, 0)) || time.isBefore(LocalTime.of(6, 0));
     }
 
--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarConfiguration.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarConfiguration.java
@@ -3,25 +3,33 @@ package com.kodilla.spring.basic.spring_configuration.homework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+import java.time.Month;
 
 
 @Configuration
 public class CarConfiguration {
 
     @Bean
-    public Car getCar(Season season) {
+    public Car getCar() {
+
+        LocalDate localDate = LocalDate.now();
+        Month month = localDate.getMonth();
 
         Car car = null;
 
-        switch (season) {
-                case SUMMER:
+        switch (month) {
+            case JUNE:
+            case JULY:
+            case AUGUST:
                     car = new Cabrio();
                     break;
-                case WINTER:
+            case DECEMBER:
+            case JANUARY:
+            case FEBRUARY:
                     car = new SUV();
                     break;
-                case SPRING:
-                case AUTUMN:
+            default:
                     car = new Sedan();
                     break;
         }
