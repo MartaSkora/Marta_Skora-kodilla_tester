6e94a14c4c398b24145fd264887264b9b5029ca2
Marta Skóra <Martaskora1@gmail.com>
2023-12-05 20:33:29 +0100

    9.4. Testy

--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_configuration/homework/CarConfigurationTestSuite.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_configuration/homework/CarConfigurationTestSuite.java
@@ -1,8 +1,29 @@
 package com.kodilla.spring.basic.spring_configuration.homework;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 @SpringBootTest
-class CarConfigurationTestSuite {
+public class CarConfigurationTestSuite {
+
+    @Autowired
+    private Car car;
+
+    @Test
+    public void shouldReturnSUV() {
+       String result = car.getCarType();
+       assertEquals("SUV", result);
+    }
+
+    @Test
+    public void shouldHaveHeadlightsOn() {
+        boolean result = car.hasHeadlightsTurnedOn();
+        assertTrue(result);
+    }
 
 }
